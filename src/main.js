487e2a06ad7a14db283261ec3c47b4e08280f136
dimakisiv <dimakisiv@gmail.com>
2024-12-20 21:32:41 +0200

    2. В мобільному меню додайте якірні посилання 3. Для .menu вкажіть height: 100%;

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@
   const mobileMenuOpenButton = document.querySelector('.menu-button');
   const mobileMenuCloseButton = document.querySelector('.menu-close');
   const menuOverlay = document.querySelector('.menu-overlay');
+  const menuLinks = document.querySelectorAll('.menu-navigation li a');
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', toggleMenu);
+  });
 
   function toggleMenu() {
     menuOverlay.classList.toggle('is-shown');
